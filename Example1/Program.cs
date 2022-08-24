1b75c44b5c4dc5013f04a48feafdd8562c1feb57
Luba <ponochka.85@bk.com>
2022-08-24 09:08:26 +0300

    add Example 1

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -3,7 +3,7 @@
    [5, 18, 123, 6, 2] -> 1
    [1, 2, 3, 6, 2] -> 0
  */
-class Program
+/*class Program
 {
     static int[] CreaterArray(int number)
     {
@@ -50,6 +50,61 @@ class Program
         WriteArray(array);
         QuantityElements(array);
     }
-} 
+} */
 
 
+/* Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний,
+   второй и предпоследний и т.д. 
+   Результат запишите в новом массиве. [1 2 3 4 5] -> 5 8 3 */
+
+class Program
+{
+    static int[] CreaterArray(int number)
+    {
+        int[] array = new int[number];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(1, 10);
+        }
+        return array;
+    }
+
+    static void WriteArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static int[] NewArray(int[] array)
+    {
+        int size = array.Length / 2;
+        if (array.Length % 2 == 1)
+        {
+            size++;
+        }
+        int[] newArray = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            if (i != array.Length - 1 - i)
+                newArray[i] = array[i] * array[array.Length - 1 - i];
+            else
+                newArray[i] = array[i];
+        }
+        return newArray;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размер массива:");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] array = CreaterArray(size);
+        Console.Write("Исходный массив: ");
+        WriteArray(array);
+        int[] newarray = NewArray(array);
+        Console.Write("Новый массив: ");
+        WriteArray(newarray);
+    }
+}
